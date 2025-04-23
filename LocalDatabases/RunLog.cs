3c33e2d3ac68a0a94109713522eed06150327b70
Jack <jack@jackb.me>
2025-04-22 23:04:48 -0400

    Enhance barcode and label builder functionality
    - Updated `BarcodeBuilderViewModel` to call `GenerateMils()` on DPI change and to generate a bitmap after barcode properties are set.
    - Refactored `CreateBitmap` to create a bitmap based on symbol size and target DPI.
    - Introduced `LabelBuilderViewModel` for integration with barcode functionalities.
    - Created `LabelBuilderView` user control for the barcode builder interface.
    - Added `RunLogDatabase` and `RunLogReportData` classes for database management.
    - Updated project files to include necessary package and project references.
    - Modified `App.xaml.cs` to process run log data and generate reports.
    - Enhanced `TemplateSelector` and `HamburgerMenu` for new label builder navigation.

---
+++ LocalDatabases/RunLog.cs
@@ -0,0 +1,73 @@
+﻿using Logging.lib;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace V275_REST_Lib.LocalDatabases
+{
+    public class RunLogDatabase
+    {
+        private bool disposedValue;
+
+        private SQLiteConnection Connection { get; set; } = null;
+
+        public RunLogDatabase() { }
+        public RunLogDatabase(string dbFilePath) => Open(dbFilePath);
+        public RunLogDatabase Open(string dbFilePath)
+        {
+            Logger.LogInfo($"Opening Database: {dbFilePath}");
+
+            if (string.IsNullOrEmpty(dbFilePath))
+                return null;
+            try
+            {
+                Connection ??= new SQLiteConnection(dbFilePath);
+
+                _ = Connection.CreateTable<RunLogReportData>();
+
+
+                return this;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+                return null;
+            }
+        }
+        public void Close() => Connection?.Dispose();
+
+        public List<RunLogReportData> SelectAllRunEntries() => Connection.CreateCommand("select * from reportData").ExecuteQuery<RunLogReportData>();
+ 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Connection?.Dispose();
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~RunDatabase()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ LocalDatabases/RunLogReportData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace V275_REST_Lib.LocalDatabases
+{
+    public class RunLogReportData
+    {
+        public int cycleId { get; set; }
+        public int cyclePassed { get; set; }
+        public string imageUrl { get; set; }
+        public string reportData { get; set; }
+        public string timeStamp { get; set; }
+        public int voidId { get; set; }
+    }
+}
